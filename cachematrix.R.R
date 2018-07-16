4383ead39784f37fbda250b18858a6f5fdd2f7fd
yunying0829 <40311970+yunying0829@users.noreply.github.com>
2018-07-16 06:02:09 -0400

    Yunying/ProgrammingAssignment2

---
+++ cachematrix.R.R
@@ -0,0 +1,31 @@
+## These functions would calculate the inverse of a matrix
+
+
+
+## This functions would give a matrix
+makeMatrix <- function(x = matrix()) {
+  i <- NULL
+  set <- function(y) {
+    x <<- y
+    i <<- NULL
+  }
+  get <- function() x
+  setinverse <- function(inverse) i <<- inverse
+  getinverse <- function() i
+  list(set = set, get = get,
+       setinverse = setinverse,
+       getinverse = getinverse)
+}
+
+## This function would calculate the inverse of a matrix
+cacheSolve <- function(x, ...) {
+  i <- x$getinverse()
+  if(!is.null(i)) {
+    message("getting cached data")
+    return(i)
+  }
+  data <- x$get()
+  i <- solve(data, ...)
+  x$setinverse(i)
+  i
+}
